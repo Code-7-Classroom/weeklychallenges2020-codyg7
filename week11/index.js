5c08702a3fedcc65f284093b7de0522c838efaa0
Cody Gilliam <codygilliam@macbook-pro.attlocal.net>
2020-04-28 21:51:49 -0400

    pushing week 11 challenges

--- week11/index.js
+++ week11/index.js
@@ -1,6 +1,6 @@
 let express = require("express");
 let app = express();
-let data = require("./public/employees.json");
+let data = require("./models/employees.json");
 
 app.get("/employees", (req, res) => {
   if (!data) {
@@ -9,16 +9,16 @@ app.get("/employees", (req, res) => {
   res.send(data);
 });
 app.get("/employees/:Employeeid", function (req, res) {
-  const sData = data.employee.find(function (employee) {
-    return parseInt(req.params.id) === employee.id;
+  const eData = data.employee.find(function (employee) {
+    return parseInt(req.params.id) === employee.Employeeid;
   });
-  if (!sData) {
+  if (!eData) {
     res.status(404).send(`Couldnt find the employees id`);
   }
-  res.send(sData);
+  res.send(eData);
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`Server running on ${port}`);
 });
